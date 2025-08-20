4142b2681667a1b54c2d5056a323e16a3d419097
¨Javier¨ <javier.gloriar@uanl.edu.mx>
2025-08-20 08:26:16 -0600

    Laboratorio 3 de Met.es, 20/08/2025

---
+++ Laboratorio_3.R
@@ -0,0 +1,18 @@
+# Laboratorio semana 3
+# 20/08/2025
+
+
+# Importar datos ----------------------------------------------------------
+
+Temp <- read.csv("temperatura.csv", header = T)
+Temp <- read.csv("Data/medias_temp.csv", header = T)
+
+# Graficar datos ----------------------------------------------------------
+
+
+
+
+
+# Estadisticas descriptivas -----------------------------------------------
+
+
