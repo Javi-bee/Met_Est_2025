ff67ef9a84bb822320f0901e84ff7315c5db3748
¨Javier¨ <javier.gloriar@uanl.edu.mx>
2025-08-27 08:51:41 -0600

    Laboratorio 3

--- Laboratorio_3.R
+++ Laboratorio_3.R
@@ -1,10 +1,12 @@
 # Laboratorio semana 3
 # 20/08/2025
+# Javier Elias Gloria Rodriguez
 
-install.packages("readr")
+# Correr (si no se tienen descargados) los paquetes dentro de la consola y hacer
+## Library() dentro de mi espacio de trabajo (script)
+
 library(readr)
 
-install.packages("repmis")
 library(repmis)
 
 # Importar datos ----------------------------------------------------------
@@ -21,8 +23,8 @@ alumno <- seq(1,18,1)
 info <- data.frame(alumno, edad)
 
 info$Altura <- c(174, 174, 170, 160, 158, 155, 188,
-                170, 175, 170, 172, 170, 174, 180,
-                158, 161, 188, 164)
+                 170, 175, 170, 172, 170, 174, 180,
+                 158, 161, 188, 164)
 
 
 # Graficar datos ----------------------------------------------------------
@@ -32,26 +34,29 @@ boxplot(info$Altura,
         col = "cornflowerblue",
         # main sirve para poner titulo a la grafica
         main = "Clase 3 semestre")  
-colors()
+
+# Este comando me da una lista entera de los colores en R colors()
 
 colores = c("indianred", "navajowhite", "skyblue")
 
-boxplot(datos_meses,
-        col = colores)
-
+# Datos obtenidos de una URL de manera seguida sin comas
 url <- "https://repodatos.atdt.gob.mx/api_update/senasica/actividades_inspeccion_movilizacion/29_actividades-inspeccion-movilizacion.csv"
 
 inspeccion <- read.csv(url)
 
 head(inspeccion)
 
+# Datos obtenidos de una URL con comas de por medio
+# No afecta a la lectura de la URL, ademas de no extenderse demasiado 
+# por el script
 prof_url_2 <- paste0("https://repodatos.atdt.gob.mx/api_update/senasica/",
-                       "actividades_inspeccion_movilizacion/",
-                       "29_actividades-inspeccion-movilizacion.csv")
+                     "actividades_inspeccion_movilizacion/",
+                     "29_actividades-inspeccion-movilizacion.csv")
 
 senasica <- read.csv(prof_url_2)
 
 head(senasica)
+# Lectura de datos.csv proveniente de la pagina dropbox
 conjunto <- source_data("https://www.dropbox.com/s/hmsf07bbayxv6m3/cuadro1.csv?dl=1")
 
 head(conjunto)
@@ -69,10 +74,14 @@ inventario$Diametro
 
 # Estadisticas descriptivas -----------------------------------------------
 
-gl(3,10)
-parcelas <- gl(3,10)
+# gl no sirve para dar diversos niveles a un conjunto de datos
+# En este caso, como se tienen 30 datos, 6 niveles de 5 datos cada una
+gl(6,5)
+parcelas <- gl(6,5)
 parcelas
 
+# En el inventario original se tenian 50 datos, con el comando seq se
+# seleccionaron datos del 1 hasta el 30
 trees <- seq(1,30)
 
 dbh<-c(16.5,25.3,22.1,17.2,16.1,8.1,34.3,5.4,5.7,11.2,24.1,
@@ -83,18 +92,35 @@ trees <- data.frame(trees, dbh, parcelas)
 
 View(trees)
 
+# Media del diametro de los arboles
 mean(trees$dbh)
+# Desviacion media de los arboles
 sd(trees$dbh)
 
+#Suma de los valores del diametro de los arboles los cuales son menores
+# a 10 cm
 sum(trees$dbh < 10)
+# El comando which nos devuelve cuales fueron los arboles que tenian un diametro
+# menor a 10 cm
 which(trees$dbh < 10)
 
+# Con este comando se puede excluir, en este caso, los arboles de la parcela
+# numero 2
 trees.13 <- trees[!(trees$parcelas=="2"),]
 trees.13
 
+# Se crea un set de datos el cual tome como consideracion 
 trees.1 <-subset(trees, dbh <=10)
 head(trees.1)
+trees.1
 
+hist(trees$dbh, 
+     col = "tomato",
+     main = "Muestra original trees")
+
+hist(trees.1$dbh,
+     col = "skyblue",
+     main = "dbh < 10 cm. trees.1")
 
 # Histogramas -------------------------------------------------------------
 
@@ -114,7 +140,21 @@ head(chickwts[c(1:2,42:43, 62:64),])
 feeds <- table(chickwts$feed)
 feeds
 
-barplot(feeds)
-barplot(feeds[order(feeds, decreasing = TRUE)])
+row.names(feeds)
 
+barplot(feeds,
+        col =  "red")
+
+barplot(feeds[order(feeds, decreasing = TRUE)],
+        col = "orange")
+
+barplot(feeds[order(feeds, decreasing = TRUE)],
+        names.arg = c("casein",    "horsebean", "linseed",
+                      "meatmeal",  "soybean", "sunflower"), horiz = TRUE,
+        col = "cornflowerblue",
+        xlab = substitute(paste(bold("Tipo de alimentacion de los polluelos"))),
+        ylab = substitute(paste(bold("Cantidad de los polluelo"))),
+)  
+
+help("barplot")
 library(tinytex)
